3755c2d6dcc2d6e838d4b9ba8be95e8b919e2456
sg777 <sarath.ginjupalli89@gmail.com>
2019-05-06 17:15:19 +0530

    debugging

--- client/pangea.js
+++ client/pangea.js
@@ -61,11 +61,14 @@ pangea.update = function(){
   for (var seat in pangea.seats){
     pangea.seats[seat].update()
   }
+  pangea.gui.addPlayerControls()	
+  /*
   if (pangea.player.sitting != 0){
     pangea.gui.addPlayerControls()
   }  else {
     pangea.gui.addJoinLabel()
   }
+  */
   pangea.gui.updateOptions()
   pangea.gui.hideSeats()
   pangea.gui.updatePotAmount()
